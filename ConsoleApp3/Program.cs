e550e10cd9fdedc8a1fe8062ece99a5eaad91b8a
jagroop98 <jagroop111298@gmail.com>
2019-01-23 11:52:29 -0500

    new chne

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            int i = 0;
+            while ( i<5)
+            {
+                i++;
+                sum += i;
+            }
+            Console.WriteLine("sum IS {0}" , sum);
+            Console.ReadLine();
+            //C0730507 JAGROOP SINGH
         }
     }
 }
